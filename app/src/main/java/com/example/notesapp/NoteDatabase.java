c71bf6c28fe140a27e82619ddfcf04f8b27eb379
adiig7 <54351909+adiig7@users.noreply.github.com>
2020-08-16 13:40:16 +0530

    new commit

---
+++ app/src/main/java/com/example/notesapp/NoteDatabase.java
@@ -0,0 +1,13 @@
+package com.example.notesapp;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Note.class}, version = 1)
+public abstract class NoteDatabase extends RoomDatabase {
+
+    private static NoteDatabase instance;
+
+    public abstract NoteDao noteDao();
+
+}
